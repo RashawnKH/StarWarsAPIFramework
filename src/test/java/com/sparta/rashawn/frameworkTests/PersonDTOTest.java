9056bda8144a470f84d09411e174ac23f9a506c1
RashawnKH <rashawn1999@hotmail.com>
2021-01-14 15:44:36 +0000

    Added tests for new functionality

--- src/test/java/com/sparta/rashawn/frameworkTests/PersonDTOTest.java
+++ src/test/java/com/sparta/rashawn/frameworkTests/PersonDTOTest.java
@@ -1,6 +1,6 @@
 package com.sparta.rashawn.frameworkTests;
 
-import framework.dtos.PersonDTO;
+import framework.dtos.*;
 import framework.injector.Injector;
 import org.junit.jupiter.api.*;
 
@@ -46,6 +46,47 @@ public class PersonDTOTest {
         Assertions.assertEquals(person.getFilms().toString(), testList.toString());
     }
 
+    @Test
+    void canGetFilmObjects(){
+        ArrayList<FilmsDTO> films = person.getFilmObjectsList();
 
+        for (FilmsDTO filmsDTO : films){
+            Assertions.assertEquals(filmsDTO.getTitle(), filmsDTO.getResponse().jsonPath().get("title"));
+        }
+
+    }
+
+    @Test
+    void canGetSpeciesObjects(){
+        ArrayList<SpeciesDTO> species= person.getSpeciesObjectsList();
+        ArrayList<String> test = person.getResponse().jsonPath().get("species");
+
+
+            Assertions.assertEquals(species.size(), test.size());
+    }
+
+
+    @Test
+    void canGetVehicles(){
+        ArrayList<VehiclesDTO> vehicles = person.getVehiclesObjectsList();
+        ArrayList<String> test = person.getResponse().jsonPath().get("vehicles");
+
+        Assertions.assertEquals(vehicles.size(), test.size());
+
+        for (VehiclesDTO vehiclesDTO : vehicles){
+            Assertions.assertEquals(vehiclesDTO.getName(), vehiclesDTO.getResponse().jsonPath().get("name"));
+        }
+    }
+
+    @Test
+    void canGetStarShips(){
+        ArrayList<StarshipsDTO> starships = person.getStarshipsObjectsList();
+        ArrayList<String> test = person.getResponse().jsonPath().get("starships");
+
+        Assertions.assertEquals(starships.size(), test.size());
+        for (StarshipsDTO starshipsDTO : starships){
+            Assertions.assertEquals(starshipsDTO.getName(), starshipsDTO.getResponse().jsonPath().get("name"));
+        }
+    }
 
 }
